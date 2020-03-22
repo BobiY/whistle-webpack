777cc3c87a8884ef6f10862b9fdcb1678c06e58c
BobiY <15810856489@163.com>
2020-03-22 19:41:47 +0800

    整理代码

--- utils/entryOption.js
+++ utils/entryOption.js
@@ -6,25 +6,25 @@
  */
 const glob = require('glob');
 const path = require('path');
+
 const singleEntry = () => {  // 查找单入口文件，如果不存在，按照多入口打包
     const files = glob.sync(path.join(process.cwd(), './src/index.@(js|jsx|ts|tsx)'));
-    // console.log(files)
-    const entry = {};
-    files.forEach( item => {
-        // const match = /(\w*)\/index.(js|jsx|ts|tsx)/;
-        const entryName = "index";
-        if ( entryName ) {
-            entry[entryName] = item;
-        }
-    } );
-    return entry;
+    const handleFun = filepath => 'index';
+    return generatorEntryFiles(files, handleFun);
 }
 
 const multipEntry = () => {
     const files = glob.sync(path.join(process.cwd(), '/src/**/index.@(js|jsx|ts|tsx)'));
+    const handleFun = filepath => {
+        return item.split("src/")[1] ? item.split("src/")[1].match(match)[1] : ''
+    }
+    return generatorEntryFiles(files, handleFun);
+}
+
+const generatorEntryFiles = (files, handleFun) => {
     const entry = {};
     files.forEach( item => {
-        const entryName = item.split("src/")[1] ? item.split("src/")[1].match(match)[1] : '';
+        const entryName = handleFun(item);
         if ( entryName ) {
             entry[entryName] = item;
         }
